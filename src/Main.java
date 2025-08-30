dd9569a880e109fdef89563a71c1634be559d0d6
Gianfranco05 <gianfrancocanciani@gmail.com>
2025-08-30 17:39:05 -0300

    agregado lombok al proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        Domicilio miCasa = Domicilio.builder()
+                .calle("Calle falsa")
+                .numero(123)
+                .build();
 
+        Persona yo = Persona.builder()
+                .nombre("Juan")
+                .edad(30)
+                .domicilio(miCasa)
+                .build();
 
-                Domicilio miCasa = new Domicilio("Calle Falsa", 123);
-                Persona yo = new Persona("Juan", 30, miCasa);
+        miCasa.setResidente(yo);
 
-                // Ahora, esta llamada funciona correctamente sin recursividad
-                System.out.println(yo);
-                // Salida: Persona{nombre='Juan', edad=30, domicilio=Domicilio{calle='Calle Falsa', numero=123}}
-            }
-        }
+        System.out.println(yo);
+    }
+}
